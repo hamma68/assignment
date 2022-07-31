0e83128415caf158bf39fd3caf9f4c27bf14af2b
hamma68 <santoshhammad10@gmail.com>
2022-07-31 23:18:36 +0530

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component, VERSION } from '@angular/core';
-
+import { DataService } from './data.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  images: any;
+  constructor(private data: DataService) {}
+  ngOnInit() {
+    this.data.getData().subscribe((res) => {
+      this.images = res;
+      console.log(this.images);
+    });
+  }
+  delete(delItem) {
+    let index;
+    for (let i = 0; i < this.images.length; i++) {
+      if (this.images[i].id === delItem) index = i;
+    }
+    console.log(index);
+    this.images.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [DataService],
 })
-export class AppModule { }
+export class AppModule {}
